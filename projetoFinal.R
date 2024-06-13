cbdb7f192f8af0d5ea92800fc19924fb1298d2b5
Vitor Guedes <126086115+VitorGuedes22@users.noreply.github.com>
2024-06-13 15:12:27 -0300

    T2 completo

--- projetoFinal.R
+++ projetoFinal.R
@@ -62,3 +62,38 @@ ggplot(summary_stats, aes(x = Age, y = mean_salary)) +
         axis.title = element_text(size = 12, face = "bold"),
         axis.text = element_text(size = 10),
         legend.position = "bottom")
+
+#### TOPICO 3
+# Adicionar a coluna Is_top_5_League à tabela rawWages
+rawWages$Is_top_5_League <- wagesClaned$Is_top_5_League
+
+# Criar uma coluna que categoriza as ligas em Top 5 Ligas e Outras Ligas
+rawWages$League_Category <- ifelse(rawWages$Is_top_5_League, "Top 5 Ligas", "Outras Ligas")
+
+# Remover caracteres não numéricos e converter para numérico
+rawWages$Salary <- as.numeric(gsub("[^0-9]", "", rawWages$Salary))
+
+# Dividir os dados em duas categorias
+salarios_top5 <- rawWages$Salary[rawWages$League_Category == "Top 5 Ligas"]
+salarios_outras <- rawWages$Salary[rawWages$League_Category == "Outras Ligas"]
+
+teste_t <- t.test(salarios_top5, salarios_outras)
+
+# Imprimir os resultados do teste t
+print(teste_t)
+# Criar o boxplot
+ggplot(rawWages, aes(x = League_Category, y = Salary, fill = League_Category)) +
+  geom_boxplot(outlier.size = 2, size = 1, width = 0.5) +  # Ajustar o tamanho das linhas das caixas
+  labs(title = "Comparação de Salários: Top 5 Ligas vs Outras Ligas",
+       x = "Categoria da Liga",
+       y = "Salário") +
+  scale_fill_manual(values = c("Top 5 Ligas" = "blue", "Outras Ligas" = "gray")) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        axis.title = element_text(size = 12, face = "bold"),
+        axis.text = element_text(size = 10),
+        legend.position = "none",
+        panel.grid.major = element_blank(),  # Remover linhas de grade principais
+        panel.grid.minor = element_blank(),  # Remover linhas de grade secundárias
+        panel.border = element_blank(),      # Remover borda do painel
+        axis.line = element_line(size = 0.5, colour = "black"))  # Ajustar linhas dos eixos
